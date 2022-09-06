119cf1aac1b45bbf8fad37f017f702594e2f788b
1998code <54872601+1998code@users.noreply.github.com>
2022-09-06 13:18:16 +0800

    Initial Commit

---
+++ ScenePhase/ScenePhaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenePhaseApp.swift
+//  ScenePhase
+//
+//  Created by Ming on 6/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScenePhaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
